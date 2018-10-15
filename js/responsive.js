939921352e5d2082d4cf9e10c1278c7355a158e3
Braiden Peterson <braidensp@me.com>
2018-10-14 19:24:07 -0700

    Basic page structures

---
+++ js/responsive.js
@@ -0,0 +1,8 @@
+function document.getItemByClassName("dropbtn") {
+    var button = document.getElementsByClassName("dropbtn")
+    var dropcontent = document.getElementsByClassName
+}
+else {
+
+}
+}
